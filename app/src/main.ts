e53bff2fcf586ca79f81051b1c02e81624257c6f
Chirag <chirag.jain1@bfhl.in>
2024-09-11 19:18:20 +0530

    refactor

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,9 +1,6 @@
-import mongoose from "mongoose";
 import { AppModule } from "./app.module";
-import { QueueService } from "queue-service";
 
 async function bootstrap() {
 	await AppModule.init();
 }
-mongoose.set("debug", true);
 bootstrap();
